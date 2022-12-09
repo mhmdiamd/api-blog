423bc7b1c8b3e0f0e550e74e3eb148d992d42557
mhmdiamd <darmawanilham34@gmail.com>
2022-12-09 18:48:59 +0700

    second push - fix route middleware post

--- Routes/post.routes.js
+++ Routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createPost, deletePost, getPost, getPosts, updatePost } from '../Controller/PostController.js';
+import { verifyToken, verifyUser } from '../Middleware/verify.js';
 const route = Router();
 
-route.post('/', createPost);
+route.post('/', verifyToken, createPost);
 route.get('/', getPosts);
 route.get('/:slug', getPost);
-route.put('/:slug', updatePost);
-route.delete('/:postId', deletePost);
+route.put('/:slug', verifyUser, updatePost);
+route.delete('/:postId', verifyUser, deletePost);
 
 export default route;
